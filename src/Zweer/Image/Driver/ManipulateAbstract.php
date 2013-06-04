087081c674619834d012565bfb9090c87879fd73
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-04 18:00:17 +0200

    Added the modify wrapper

--- src/Zweer/Image/Driver/ManipulateAbstract.php
+++ src/Zweer/Image/Driver/ManipulateAbstract.php
@@ -19,4 +19,37 @@ abstract class ManipulateAbstract implements ManipulateInterface
     {
         $this->_image = $image;
     }
+
+    /**
+     * Modify wrapper
+     * Used in many function such as resize and grab
+     *
+     * @param int $dst_x
+     * @param int $dst_y
+     * @param int $src_x
+     * @param int $src_y
+     * @param int $dst_w
+     * @param int $dst_h
+     * @param int $src_w
+     * @param int $src_h
+     *
+     * @return ManipulateInterface
+     */
+    protected function modify($dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h)
+    {
+        // create new image
+        $image = imagecreatetruecolor($dst_w, $dst_h);
+
+        // preserve transparency
+        imagealphablending($image, false);
+        imagesavealpha($image, true);
+
+        // copy content from resource
+        imagecopyresampled($image, $this->_image->getResource(), $dst_x , $dst_y , $src_x , $src_y , $dst_w , $dst_h , $src_w , $src_h);
+
+        // set new content as recource
+        $this->_image->setResource($image);
+
+        return $this;
+    }
 }
