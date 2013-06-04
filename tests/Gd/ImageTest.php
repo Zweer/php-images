fae5bd59e015d7f039923cda54dd0b09f04c29e3
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-04 17:44:44 +0200

    Removed the orientation tests

--- tests/Gd/ImageTest.php
+++ tests/Gd/ImageTest.php
@@ -19,25 +19,5 @@ class ImageTest extends PHPUnit_Framework_TestCase
 
         $this->assertInternalType('int', $img->getHeight());
         $this->assertEquals($width, $img->getHeight());
-
-        $this->assertInternalType('string', $img->getOrientation());
-        $this->assertEquals(Image::ORIENTATION_SQUARE, $img->getOrientation());
-    }
-
-    public function testContructorEmptyPortrait()
-    {
-        $width = 2;
-        $height = 3;
-
-        $img = Image::create($width, $height);
-
-        $this->assertInternalType('int', $img->getWidth());
-        $this->assertEquals($width, $img->getWidth());
-
-        $this->assertInternalType('int', $img->getHeight());
-        $this->assertEquals($height, $img->getHeight());
-
-        $this->assertInternalType('string', $img->getOrientation());
-        $this->assertEquals(Image::ORIENTATION_PORTRAIT, $img->getOrientation());
     }
 }
