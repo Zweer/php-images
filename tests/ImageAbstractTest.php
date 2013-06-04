3697162c63b03b0986d4f65e60a5eb442840bb93
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-04 17:47:55 +0200

    Added the orientation tests

---
+++ tests/ImageAbstractTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Zweer\Image\Image;
+
+class ImageAbstractTest extends PHPUnit_Framework_TestCase
+{
+    public function testSquared()
+    {
+        $width = 2;
+
+        $img = Image::create($width);
+
+        $this->assertInternalType('string', $img->getOrientation());
+        $this->assertEquals(Image::ORIENTATION_SQUARE, $img->getOrientation());
+    }
+
+    public function testPortraite()
+    {
+        $width = 2;
+        $height = 3;
+
+        $img = Image::create($width, $height);
+
+        $this->assertInternalType('string', $img->getOrientation());
+        $this->assertEquals(Image::ORIENTATION_PORTRAIT, $img->getOrientation());
+    }
+
+    public function testLandscape()
+    {
+        $width = 2;
+        $height = 1;
+
+        $img = Image::create($width, $height);
+
+        $this->assertInternalType('string', $img->getOrientation());
+        $this->assertEquals(Image::ORIENTATION_LANDSCAPE, $img->getOrientation());
+    }
+}
