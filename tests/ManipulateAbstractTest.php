1a5e185d498eef927b9bae3807e8b7e9e541e549
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-05 09:52:38 +0200

    Added the upsized resize test

--- tests/ManipulateAbstractTest.php
+++ tests/ManipulateAbstractTest.php
@@ -25,5 +25,13 @@ class ManipulateAbstractTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($resizeWidth, $img->getWidth());
         $this->assertEquals($resizeHeight, $img->getHeight());
+
+        $resizeWidth *= 2;
+        $resizeHeight *= 2;
+
+        $img->manipulate()->resize($resizeWidth, $resizeHeight, true, true);
+
+        $this->assertEquals($resizeWidth, $img->getWidth());
+        $this->assertEquals($resizeHeight, $img->getHeight());
     }
 }
