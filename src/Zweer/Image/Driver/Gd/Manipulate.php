dcf680dc02f5fa2896613763c8e3c430367aaff8
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-05 11:04:10 +0200

    Modified the _modify method to use the copy method of the ImageInterface

--- src/Zweer/Image/Driver/Gd/Manipulate.php
+++ src/Zweer/Image/Driver/Gd/Manipulate.php
@@ -12,31 +12,23 @@ class Manipulate extends ManipulateAbstract
      * Modify wrapper
      * Used in many function such as resize and grab
      *
-     * @param int $dst_x Destination X coord
-     * @param int $dst_y Destination Y coord
-     * @param int $src_x Source X coord
-     * @param int $src_y Source Y coord
-     * @param int $dst_w Destination width
-     * @param int $dst_h Destination height
-     * @param int $src_w Source width
-     * @param int $src_h Source height
+     * @param int  $destinationX      Destination X coord
+     * @param int  $destinationY      Destination Y coord
+     * @param int  $sourceX           Source X coord
+     * @param int  $sourceY           Source Y coord
+     * @param int  $destinationWidth  Destination width
+     * @param int  $destinationHeight Destination height
+     * @param int  $sourceWidth       Source width
+     * @param int  $sourceHeight      Source height
+     * @param null $bgColor           The color to use as the background
      *
      * @return ManipulateInterface
      */
-    protected function _modify($dst_x, $dst_y, $src_x, $src_y, $dst_w, $dst_h, $src_w, $src_h)
+    protected function _modify($destinationX, $destinationY, $sourceX, $sourceY, $destinationWidth, $destinationHeight, $sourceWidth, $sourceHeight, $bgColor = null)
     {
-        // create new image
-        $image = imagecreatetruecolor($dst_w, $dst_h);
+        $newImage = ImageWrapper::create($destinationWidth, $destinationHeight, $bgColor);
 
-        // preserve transparency
-        imagealphablending($image, false);
-        imagesavealpha($image, true);
-
-        // copy content from resource
-        imagecopyresampled($image, $this->_image->getResource(), $dst_x , $dst_y , $src_x , $src_y , $dst_w , $dst_h , $src_w , $src_h);
-
-        // set new content as recource
-        $this->_image->setResource($image);
+        $this->_image->copy($newImage, $destinationX, $destinationY, $sourceX, $sourceY, $destinationWidth, $destinationHeight, $sourceWidth, $sourceHeight);
 
         return $this;
     }
