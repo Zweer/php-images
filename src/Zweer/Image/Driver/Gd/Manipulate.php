19122d84e9577f3c1eaf80e28239cd5ebe8662b6
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-05 09:59:22 +0200

    Modified the PHPDoc to allow also relative resizing

--- src/Zweer/Image/Driver/Gd/Manipulate.php
+++ src/Zweer/Image/Driver/Gd/Manipulate.php
@@ -73,9 +73,13 @@ class Manipulate extends ManipulateAbstract
     /**
      * Resizes the image
      * You can decide if preserving the ratio and upsizing the image.
+     * $width and $height can be specified relative to the actual image size:
+     * - '+2' is 2 pixel more than the actual size;
+     * - '-2' (both an int or a string) is 2 pixel less than the actual size
+     * - '2%' is the percentage of the actua size
      *
-     * @param int  $width
-     * @param int  $height
+     * @param int|string  $width
+     * @param int|string  $height
      * @param bool $ratio
      * @param bool $upsize
      *
