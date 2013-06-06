f514ba64dd8a7fffdda0f263881442f4f0d121e1
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-06 10:22:54 +0200

    Added the EngineAbstract

---
+++ src/Zweer/Image/Driver/EngineAbstract.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Zweer\Image\Driver;
+
+abstract class EngineAbstract implements EngineInterface
+{
+    /**
+     * @var ImageInterface
+     */
+    protected $_image;
+
+    /**
+     * The contructor
+     * It only stores the $image argument.
+     *
+     * @param ImageInterface $image
+     */
+    public function __construct(ImageInterface $image)
+    {
+        $this->_image = $image;
+    }
+}
