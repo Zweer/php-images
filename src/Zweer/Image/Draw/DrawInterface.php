9e54b9e8c8fc336e06a332d151f202e2e8eb536a
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-06 11:05:13 +0200

    Added the rectangle method

--- src/Zweer/Image/Draw/DrawInterface.php
+++ src/Zweer/Image/Draw/DrawInterface.php
@@ -6,5 +6,18 @@ use Zweer\Image\Engine\EngineInterface;
 
 interface DrawInterface extends EngineInterface
 {
-
+    /**
+     * Draws a rectangle
+     * It starts at ($x1, $y1) and ends at ($x2, $y2)
+     *
+     * @param string|array $color
+     * @param int          $x1
+     * @param int          $y1
+     * @param int          $x2
+     * @param int          $y2
+     * @param bool         $filled
+     *
+     * @return DrawInterface
+     */
+    public function rectangle($color, $x1 = 0, $y1 = 0, $x2 = 10, $y2 = 10, $filled = true);
 }
--- src/Zweer/Image/Driver/Gd/Draw.php
+++ src/Zweer/Image/Driver/Gd/Draw.php
@@ -3,8 +3,28 @@
 namespace Zweer\Image\Driver\Gd;
 
 use Zweer\Image\Draw\DrawAbstract;
+use Zweer\Image\Draw\DrawInterface;
 
 class Draw extends DrawAbstract
 {
+    /**
+     * Draws a rectangle
+     * It starts at ($x1, $y1) and ends at ($x2, $y2)
+     *
+     * @param string|array $color
+     * @param int          $x1
+     * @param int          $y1
+     * @param int          $x2
+     * @param int          $y2
+     * @param bool         $filled
+     *
+     * @return DrawInterface
+     */
+    public function rectangle($color, $x1 = 0, $y1 = 0, $x2 = 10, $y2 = 10, $filled = true)
+    {
+        $callback = $filled ? 'imagefilledrectangle' : 'imagerectangle';
+        call_user_func($callback, $this->_image->getResource(), $x1, $y1, $x2, $y2, $this->_image->allocateColor($color));
 
+        return $this;
+    }
 }
