913a2e8b6a1614e5f26802481830998dffd9600d
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-06 10:26:39 +0200

    Obviously without the inherited methods

--- src/Zweer/Image/Driver/ManipulateInterface.php
+++ src/Zweer/Image/Driver/ManipulateInterface.php
@@ -5,14 +5,6 @@ namespace Zweer\Image\Driver;
 interface ManipulateInterface extends EngineInterface
 {
     /**
-     * The contructor
-     * It only stores the $image argument.
-     *
-     * @param ImageInterface $image
-     */
-    public function __construct(ImageInterface $image);
-
-    /**
      * Returns the string representation of the image
      *
      * @return string
