d43f8028fb43ce4855bac3e187df612d98e2aba3
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-07 15:22:59 +0200

    Changes to improve coverage

--- tests/ManipulateAbstractTest.php
+++ tests/ManipulateAbstractTest.php
@@ -13,21 +13,29 @@ class ManipulateAbstractTest extends PHPUnit_Framework_TestCase
 
     public function testFlip()
     {
-        $width = 20;
-        $height = 30;
+        $img = Image::make(__DIR__ . '/../examples/assets/ralph.jpg');
+        $width = $img->getWidth();
+        $height = $img->getHeight();
+        $x = 700;
+        $y = 300;
+        $color = $img->pickColor($x, $y, 'int');
 
-        $img = Image::create($width, $height);
         $img->manipulate()->flip(Image::FLIP_HORIZONTAL);
 
         $this->assertEquals($width, $img->getWidth());
         $this->assertEquals($height, $img->getHeight());
+        $this->assertEquals($color, $img->pickColor($width - $x - 1, $y, 'int'));
 
         $img->manipulate()->flip(Image::FLIP_VERTICAL);
 
         $this->assertEquals($width, $img->getWidth());
         $this->assertEquals($height, $img->getHeight());
+        $this->assertEquals($color, $img->pickColor($width - $x - 1, $height - $y - 1, 'int'));
     }
 
+    /**
+     * @expectedException \Exception
+     */
     public function testResize()
     {
         $originalWidth = 20;
@@ -62,6 +70,24 @@ class ManipulateAbstractTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($resizeWidth, $img->getWidth());
         $this->assertEquals($resizeHeight, $img->getHeight());
+
+        $img->manipulate()->resize($resizeWidth + 1, $resizeHeight + 10, true, false);
+
+        $this->assertEquals($resizeWidth, $img->getWidth());
+        $this->assertEquals($resizeHeight, $img->getHeight());
+
+        $img->manipulate()->resize($resizeWidth + 1, null, false, false);
+
+        $this->assertEquals($resizeWidth, $img->getWidth());
+        $this->assertEquals($resizeHeight, $img->getHeight());
+
+        $img->manipulate()->resize(null, $resizeHeight + 10, false, false);
+
+        $this->assertEquals($resizeWidth, $img->getWidth());
+        $this->assertEquals($resizeHeight, $img->getHeight());
+
+        // Throws an expected exception
+        $img->manipulate()->resize(null, null, false, false);
     }
 
     public function testRelativeResize()
@@ -81,7 +107,7 @@ class ManipulateAbstractTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($resizedWidth, $img->getWidth());
         $this->assertEquals($resizedHeight, $img->getHeight());
 
-        $img->manipulate()->resize('50%');
+        $img->manipulate()->resize('50%', '50%');
 
         $this->assertEquals(intval($resizedWidth / 2), $img->getWidth());
         $this->assertEquals(intval($resizedHeight / 2), $img->getHeight());
@@ -100,6 +126,24 @@ class ManipulateAbstractTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals($resizeWidth, $img->getWidth());
         $this->assertEquals($resizeHeight, $img->getHeight());
+
+        $resizeWidth = $originalWidth;
+        $resizeHeight = $originalHeight;
+
+        $img->manipulate()->canvas($resizeWidth, $resizeHeight);
+
+        $this->assertEquals($resizeWidth, $img->getWidth());
+        $this->assertEquals($resizeHeight, $img->getHeight());
+    }
+
+    public function testCanvasAnchor()
+    {
+        $width = 20;
+
+        $img = Image::create($width, null, '000');
+        $img->manipulate()->canvas($width + 1, null, 'top left', 'fff');
+
+        $this->assertEquals('#ffffff', strtolower($img->pickColor($width + 1, 0, 'hex')));
     }
 
     public function testCrop()
