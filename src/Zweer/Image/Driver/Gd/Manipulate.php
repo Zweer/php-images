e96d6d9b32b5ab09805d5562fd8a86f26efb6dd6
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-07 14:35:03 +0200

    Changed the _modify method

--- src/Zweer/Image/Driver/Gd/Manipulate.php
+++ src/Zweer/Image/Driver/Gd/Manipulate.php
@@ -12,23 +12,30 @@ class Manipulate extends ManipulateAbstract
      * Modify wrapper
      * Used in many function such as resize and grab
      *
-     * @param int  $destinationX      Destination X coord
-     * @param int  $destinationY      Destination Y coord
-     * @param int  $sourceX           Source X coord
-     * @param int  $sourceY           Source Y coord
-     * @param int  $destinationWidth  Destination width
-     * @param int  $destinationHeight Destination height
-     * @param int  $sourceWidth       Source width
-     * @param int  $sourceHeight      Source height
-     * @param null $bgColor           The color to use as the background
+     * @param int          $destinationX              Destination X coord
+     * @param int          $destinationY              Destination Y coord
+     * @param int          $sourceX                   Source X coord
+     * @param int          $sourceY                   Source Y coord
+     * @param int          $destinationWidth          Destination width
+     * @param int          $destinationHeight         Destination height
+     * @param int          $sourceWidth               Source width
+     * @param int          $sourceHeight              Source height
+     * @param string|array $bgColor                   The color to use as the background
+     * @param int          $sourceWidthAtDestination
+     * @param int          $sourceHeightAtDestination
      *
      * @return ManipulateInterface
      */
-    protected function _modify($destinationX, $destinationY, $sourceX, $sourceY, $destinationWidth, $destinationHeight, $sourceWidth, $sourceHeight, $bgColor = null)
+    protected function _modify($destinationX, $destinationY, $sourceX, $sourceY, $destinationWidth, $destinationHeight, $sourceWidth, $sourceHeight, $bgColor = null, $sourceWidthAtDestination = null, $sourceHeightAtDestination = null)
     {
         $newImage = ImageWrapper::create($destinationWidth, $destinationHeight, $bgColor);
 
-        $this->_image->replace($newImage, $destinationX, $destinationY, $sourceX, $sourceY, $destinationWidth, $destinationHeight, $sourceWidth, $sourceHeight);
+        $this->_image->replace($newImage,
+            $destinationX, $destinationY,
+            $sourceX, $sourceY,
+            isset($sourceWidthAtDestination) ? $sourceWidthAtDestination : $destinationWidth, isset($sourceHeightAtDestination) ? $sourceHeightAtDestination : $destinationHeight,
+            $sourceWidth, $sourceHeight
+        );
 
         return $this;
     }
