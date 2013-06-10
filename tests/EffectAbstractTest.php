f99188596fb68bb35e797bec1390bf8db0e25217
Niccolo' Olivieri <flicofloc@gmail.com>
2013-06-10 08:48:53 +0200

    Added the coverage for the sepia function

--- tests/EffectAbstractTest.php
+++ tests/EffectAbstractTest.php
@@ -9,5 +9,9 @@ class EffectAbstractTest extends PHPUnit_Framework_TestCase
         $img = Image::make(__DIR__ . '/../examples/assets/ralph.jpg');
 
         $this->assertInstanceOf('Zweer\\Image\\Effect\\EffectAbstract', $img->effect()->sepia());
+
+        $img = Image::make(__DIR__ . '/../examples/assets/ralph.gif');
+
+        $this->assertInstanceOf('Zweer\\Image\\Effect\\EffectAbstract', $img->effect()->sepia());
     }
 }
